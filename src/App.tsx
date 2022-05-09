7db01ae95c4a84debc3fba11dd03eaaacaad66fc
Mingkun Ma <mmk.ee.911@gmail.com>
2022-05-09 14:56:55 -0700

    feat: Add RTK

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 import { useAppDispatch, useAppSelector } from "./app/hooks";
 import { incremented, amountAdded } from "./features/counter/counter-slice";
+import { useFetchBreedsQuery } from "./features/dogs/dogs-api-slice";
 
 function App() {
   const count = useAppSelector((state) => state.counter.value);
   const dispatch = useAppDispatch();
+
+  const [numDogs, setNumDogs] = useState(10);
+  const { data = [], isFetching } = useFetchBreedsQuery(numDogs);
 
   function handleClick() {
     dispatch(amountAdded(3));
@@ -21,6 +25,42 @@ function App() {
         <p>
           <button onClick={handleClick}>count is: {count}</button>
         </p>
+
+        <div>
+          <p>Dogs to fetch:</p>
+          <select
+            value={numDogs}
+            onChange={(e) => setNumDogs(Number(e.target.value))}
+          >
+            <option value="5">5</option>
+            <option value="10">10</option>
+            <option value="15">15</option>
+            <option value="20">20</option>
+          </select>
+        </div>
+
+        <div>
+          <p>Number of dogs fetched: {data.length}</p>
+          <table>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Picture</th>
+              </tr>
+            </thead>
+            <tbody>
+              {data.map((breed) => (
+                <tr key={breed.id}>
+                  <td>{breed.name}</td>
+                  <td>
+                    <img src={breed.image.url} alt={breed.name} height="250" />
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+
         <p>
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
---
+++ src/features/dogs/dogs-api-slice.ts
@@ -0,0 +1,34 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const DOGS_API_KEY = "29d4bdf0-6ba2-4dc5-9d1a-78dcf0417000";
+
+interface Breed {
+  id: string;
+  name: string;
+  image: {
+    url: string;
+  };
+}
+
+export const apiSlice = createApi({
+  reducerPath: "api",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "https://api.thedogapi.com/v1",
+    prepareHeaders(headers) {
+      headers.set("x-api-key", DOGS_API_KEY);
+
+      return headers;
+    },
+  }),
+  endpoints(builder) {
+    return {
+      fetchBreeds: builder.query<Breed[], number | void>({
+        query(limit = 10) {
+          return `/breeds?limit=${limit}`;
+        },
+      }),
+    };
+  },
+});
+
+export const { useFetchBreedsQuery } = apiSlice;
